050b16bbc0cc51b4490e7b676f3be89ed7a979f2
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-10 13:37:09 -0600

    part 1.7

--- part 1/part1.6/src/App.jsx
+++ part 1/part1.6/src/App.jsx
@@ -6,6 +6,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad;
+
+  const average = (good - bad) / all ;
+
+  var positive = 100 / (good + neutral + bad) * good;
+
+  if(isNaN(positive)){
+    positive = "No reviews yet."
+  } else {
+    positive += "%"
+  }
+
   const handleGoodClick = () => {
     setGood(good + 1);
   }
@@ -28,6 +40,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
