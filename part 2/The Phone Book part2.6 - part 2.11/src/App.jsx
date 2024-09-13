77e6ff8276dc81f18983baeb470c192433ec8155
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-13 12:56:52 -0600

    2.12

--- part 2/The Phone Book part2.6 - part 2.11/src/App.jsx
+++ part 2/The Phone Book part2.6 - part 2.11/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 const FilterInput = ({filter, setFilter}) => {
   return(
@@ -44,21 +45,10 @@ const Numbers = ({personsFiltered}) => {
 const App = () => {
 
   useEffect(() =>{
-    console.log('efecting');
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-        console.log(persons)
-      })
+    personService.getAll().then((persons) => setPersons(persons))
   }, [])
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -81,11 +71,20 @@ const App = () => {
       alert('dont add void inputs')
       return
     }
-    setPersons(persons.concat(
-      {name: newName, number: newNumber, id: persons.length + 1}
-    ))
-    setNewNumber('')
-    setNewName('')
+
+    const personObjetc = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    personService.create(personObjetc).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson))
+      setNewNumber('')
+      setNewName('')
+    })
+    
+    
   }
 
   return (
