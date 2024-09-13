644f9c7d2f30ff12d35de888945c339b15549d11
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-13 15:32:51 -0600

    part 2.13 already done

--- part 2/The Phone Book part2.6 - part 2.11/src/services/persons.js
+++ part 2/The Phone Book part2.6 - part 2.11/src/services/persons.js
@@ -11,11 +11,6 @@ const getAll = () => {
     return request.then(response => response.data)
   }
   
- /*  const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-  } */
-  
   export default { 
     getAll: getAll, 
     create: create
