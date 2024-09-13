6e5dad5cbe8ea4b406c9a356480f99683a19315e
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-12 20:52:14 -0600

    part 2.8

--- part 2/The Phone Book part2.6 - part 2.10/src/App.jsx
+++ part 2/The Phone Book part2.6 - part 2.10/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleAddButton = (event) => {
     event.preventDefault()
     if(persons.some((person)=>person.name === newName)){
-      alert(`${newName} is already in the phonebook`)
+      alert(`name ${newName} is already in the phonebook`)
+      return
+    }
+    if(persons.some((person) => person.number === newNumber)){
+      alert(`number ${newNumber} is already in the phonebook`)
+      return
+    }
+    if(newName === '' || newNumber === ''){
+      alert('dont add void inputs')
       return
     }
     setPersons(persons.concat(
@@ -26,6 +35,9 @@ const App = () => {
           name: <input value={newName} onChange={(event) =>{setNewName(event.target.value)}} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => {setNewNumber(event.target.value)}} />
+        </div>
+        <div>
           <button onClick={handleAddButton} type="submit">add</button>
         </div>
       </form>
@@ -34,7 +46,7 @@ const App = () => {
         return (
           <div key={person.name}>
             <p>
-              {person.name}
+              {person.name} {person.number}
             </p>
           </div>
         )
