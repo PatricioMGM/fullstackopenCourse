8c8a0cd759d0da30f77c3b2eeb07898a7df4a027
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-14 02:34:12 -0600

    part 2.20

--- part 2/Country data part2.18 - part 2.20/src/App.jsx
+++ part 2/Country data part2.18 - part 2.20/src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Countries({ countries, handleShowButton }) {
-  if(countries.length === 0){
-    return(
-      <p>Introduce the name of a country</p>
-    )
-  }else if (countries.length === 1) {
+function Countries({ countries, handleShowButton, weather }) {
+  if (countries.length === 0) {
+    return <p>Introduce the name of a country</p>;
+  } else if (countries.length === 1) {
     return (
       <div>
+        <p>{countries[0].name.common}</p>
         <p>{countries[0].capital}</p>
         <p>{countries[0].area}</p>
         <ul>
@@ -17,6 +16,15 @@ function Countries({ countries, handleShowButton }) {
           ))}
         </ul>
         <img src={countries[0].flags.png} alt={countries[0].flags.alt} />
+        {weather && weather.main && weather.weather && weather.wind && (
+          <div>
+            <h3>Weather in {countries[0].capital}</h3>
+            <p>Temperature: {weather.main.temp} °C</p>
+            <p>Weather: {weather.weather[0].description}</p>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="weather icon" />
+            <p>Wind: {weather.wind.speed} m/s</p>
+          </div>
+        )}
       </div>
     );
   } else if (countries.length > 10) {
@@ -39,6 +47,7 @@ function App() {
   const [states, setStates] = useState([]);
   const [input, setInput] = useState('');
   const [result, setResult] = useState([]);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then((response) => setStates(response.data));
@@ -52,6 +61,18 @@ function App() {
     }
   }, [input, states]);
 
+  useEffect(() => {
+    if (result.length === 1) {
+      const capital = result[0].capital;
+      const apiKey = import.meta.env.VITE_SOME_KEY;
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`)
+        .then((response) => setWeather(response.data));
+    } else {
+      setWeather(null);
+    }
+  }, [result]);
+
   const handleShowButton = (value) => {
     setInput(value);
   };
@@ -60,7 +81,7 @@ function App() {
     <>
       <h1>Countries</h1>
       <input type="text" value={input} onInput={(event) => setInput(event.target.value)} />
-      <Countries countries={result} handleShowButton={handleShowButton} />
+      <Countries countries={result} handleShowButton={handleShowButton} weather={weather} />
     </>
   );
 }
