79bc77662ba66f54380dfcd6f1e232df1869d2c6
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-09-14 01:48:16 -0600

    part 2.19

--- part 2/Country data part2.18 - part 2.20/src/App.jsx
+++ part 2/Country data part2.18 - part 2.20/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Countries = ({countries}) =>{
+function Countries({ countries, handleShowButton }) {
   if(countries.length === 0){
-    return (
+    return(
       <p>Introduce the name of a country</p>
     )
-  }else if(countries.length === 1){
-    return(
-      <div>   
-        <p>{countries[0].name.common}</p>
+  }else if (countries.length === 1) {
+    return (
+      <div>
         <p>{countries[0].capital}</p>
         <p>{countries[0].area}</p>
         <ul>
@@ -19,40 +18,51 @@ const Countries = ({countries}) =>{
         </ul>
         <img src={countries[0].flags.png} alt={countries[0].flags.alt} />
       </div>
-    )
-  }else if(countries.length > 10){
-    return(
-      <p>Too many matches, specify another filter</p>
-    )
-  }else{
-    return(
+    );
+  } else if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else {
+    return (
       <div>
-        {countries.map((country)=>{
-          return(
-            <p key={country.name.common}>{country.name.common}</p>
-          )
-        })}
+        {countries.map((country) => (
+          <div key={country.name.common}>
+            <p>{country.name.common}</p>
+            <button onClick={() => handleShowButton(country.name.common)}>show</button>
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
 
 function App() {
   const [states, setStates] = useState([]);
   const [input, setInput] = useState('');
-  useEffect(()=>{
-    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then((response)=>setStates(response.data));
-  }, [])
-  let result = []
-  input != '' ? result = states.filter(state => state.name.common.toLowerCase().includes(input)) : null
-  
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then((response) => setStates(response.data));
+  }, []);
+
+  useEffect(() => {
+    if (input !== '') {
+      setResult(states.filter(state => state.name.common.toLowerCase().includes(input.toLowerCase())));
+    } else {
+      setResult([]);
+    }
+  }, [input, states]);
+
+  const handleShowButton = (value) => {
+    setInput(value);
+  };
+
   return (
     <>
       <h1>Countries</h1>
-      <input type="text" onInput={(event)=>setInput(event.target.value)} />
-      <Countries countries={result} />
+      <input type="text" value={input} onInput={(event) => setInput(event.target.value)} />
+      <Countries countries={result} handleShowButton={handleShowButton} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
